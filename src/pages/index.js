1e246daf3d0f5585d7956260ccda253d6bbc408d
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2020-07-08 13:19:53 +0800

    feat: add lint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,22 @@
-import React, { Component } from 'react';
-import router from 'umi/router';
-
-import styles from './index.less';
-
+import React, { Component } from 'react'
+import router from 'umi/router'
+import styles from './index.less'
 
 // console.log(process.UMI_ENV)
 class RegistrationForm extends React.Component {
   componentDidMount() {
-    console.log('aaa')
-    router.replace('/product');
-
-   
+    router.replace('/product')
   }
 
   render() {
-    return (
-      <div className={styles.normal} />
-    );
+    return <div className={styles.normal} />
   }
 }
 
 function mapStateToProps(state) {
-  const {name} = state.global;
+  const { name } = state.global
   return {
     name,
-  };
+  }
 }
-export default RegistrationForm;
+export default RegistrationForm
